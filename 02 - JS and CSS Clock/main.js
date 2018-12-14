82ed0ebea4b35f9f9afbed3a34a6b533dbeb7b69
wangfan <i.wangfan@foxmail.com>
2018-12-14 10:33:43 +0800

    update

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -9,8 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const hours = now.getHours();
         const minutes = now.getMinutes();
         const seconds = now.getSeconds();
-        const secondsDegree = (seconds / 60) * 360;
+        const secondsDegree = seconds * (360 / 60);
+        const minuteDegree = (minutes + (seconds / 60)) * (360 / 60);
+        const hoursDegree = (hours + (minutes / 60) + (seconds / 60 / 60)) * (360 / 12);
+        // 解决角度置为0， 指针跳动的问题
+        secHand.style.transition = secondsDegree === 0 ? 'transform 0s' : ' transform .8s cubic-bezier(0.43, 1.69, 0.97, 1.3)';
+        // (<HTMLDivElement>minHand).style.transitionDelay = minuteDegree === 0 ? '0' : '.8s';
+        // (<HTMLDivElement>hourHand).style.transitionDelay = hoursDegree === 0 ? '0' : '.8s';
         secHand.style.transform = `rotate(${secondsDegree}deg)`;
+        minHand.style.transform = `rotate(${minuteDegree}deg)`;
+        hourHand.style.transform = `rotate(${hoursDegree}deg)`;
     }
     setInterval(updateDate, 1000);
+    updateDate();
 });
--- 02 - JS and CSS Clock/main.ts
+++ 02 - JS and CSS Clock/main.ts
@@ -1,20 +1,30 @@
 ;document.addEventListener("DOMContentLoaded", () => {
 
-    const hourHand = document.querySelector<HTMLDivElement>('.hour-hand-hook');
-    const minHand = document.querySelector<HTMLDivElement>('.min-hand-hook');
-    const secHand = document.querySelector<HTMLDivElement>('.second-hand-hook');
+  const hourHand = document.querySelector<HTMLDivElement>('.hour-hand-hook');
+  const minHand = document.querySelector<HTMLDivElement>('.min-hand-hook');
+  const secHand = document.querySelector<HTMLDivElement>('.second-hand-hook');
 
-    function updateDate() {
-        const now = new Date();
-        const hours = now.getHours();
-        const minutes = now.getMinutes();
-        const seconds = now.getSeconds();
+  function updateDate() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
 
+    const secondsDegree = seconds * (360 / 60);
 
-        const secondsDegree = (seconds / 60) * 360;
+    const minuteDegree = (minutes + (seconds / 60)) * (360 / 60);
 
-      (<HTMLDivElement>secHand).style.transform = `rotate(${secondsDegree}deg)`;
-    }
+    const hoursDegree = (hours + (minutes / 60) + (seconds / 60 / 60)) * (360 / 12);
 
-    setInterval(updateDate, 1000);
+    // 解决角度置为0， 指针跳动的问题
+    (<HTMLDivElement>secHand).style.transition = secondsDegree === 0 ? 'transform 0s' : ' transform .8s cubic-bezier(0.43, 1.69, 0.97, 1.3)';
+    // (<HTMLDivElement>minHand).style.transitionDelay = minuteDegree === 0 ? '0' : '.8s';
+    // (<HTMLDivElement>hourHand).style.transitionDelay = hoursDegree === 0 ? '0' : '.8s';
+    (<HTMLDivElement>secHand).style.transform = `rotate(${secondsDegree}deg)`;
+    (<HTMLDivElement>minHand).style.transform = `rotate(${minuteDegree}deg)`;
+    (<HTMLDivElement>hourHand).style.transform = `rotate(${hoursDegree}deg)`;
+  }
+
+  setInterval(updateDate, 1000);
+  updateDate()
 });
